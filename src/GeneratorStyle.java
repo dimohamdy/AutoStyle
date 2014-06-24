f8d00a307a67377537dceb233ea37d5c17340311
Ahmed Hamdy <dimo.hamdy@gmail.com>
2014-06-24 12:57:13 +0200

    Docmentation
    Add comments to understand Code

--- src/GeneratorStyle.java
+++ src/GeneratorStyle.java
@@ -7,13 +7,20 @@ import org.apache.commons.io.FilenameUtils;
 import org.jdom2.Element;
 import org.jdom2.filter.ElementFilter;
 import org.jdom2.input.SAXBuilder;
-import org.jdom2.output.XMLOutputter;
+
 
 public class GeneratorStyle {
 
     static String basename = "";
     List<Connection> connections;
-
+    //GeneratorConnection Function 
+    /*
+     * 
+     * openFile the path of the Nib File That will Parse and Generate Connections From it
+     * 
+     * 
+     * 
+     */
     void GeneratorConnection(String openFile) {
         // Assume filename argument
 
@@ -23,91 +30,88 @@ public class GeneratorStyle {
             // Build the document with SAX and Xerces, no validation
             SAXBuilder builder = new SAXBuilder();
             // Create the document
-//			org.jdom2.Document document = builder
-//					.build(new File("ViewTest.xml"));
+            //open the nib file to get all the connections of Outlets
             org.jdom2.Document document = builder.build(new File(openFile));
 
-            // System.out.print(openFile);
+            // get the root of the nib file "Root Tag of XML"
             Element root = document.getRootElement();
+            //Make Filter that use to search for Tag his name is "Connections"
             ElementFilter filter = new org.jdom2.filter.ElementFilter(
                     "connections");
-            XMLOutputter fmt = new XMLOutputter();
+
+            //uiElement that use save the Childern UIElement result that match  Filtering
             List<Element> uiElements = null;
+            //
             connections = new ArrayList<Connection>();
             for (Element c : root.getDescendants(filter)) {
-                // System.out.println(c.getTextNormalize());
-                // fmt.output(c, System.out);
+                //get all Childern of Tag "Connection" 
                 uiElements = c.getChildren();
-
-
-
-                System.out.println(uiElements.size());
-
+                //Check the "Connections" Tag have "Outlet" Tag Childern OR Other Childern 
                 if (uiElements.size() > 0) {
+                    //Loop For All Childern of "Connections" Tag
                     for (int count = 0; count < uiElements.size(); count++) {
-
-                        if(uiElements.get(count).getName().equals("outlet")){
-
-                        String property = uiElements.get(count).getAttributeValue("property");
-                        String destination = uiElements.get(count).getAttributeValue("destination");
-
-                        connections.add(new Connection(property, destination));
-
+                        //Check the Child Name is Outlet
+                        if (uiElements.get(count).getName().equals("outlet")) {
+                            //use the Outlet values Property and destenation
+                            String property = uiElements.get(count).getAttributeValue("property");
+                            String destination = uiElements.get(count).getAttributeValue("destination");
+                            //Generate Connection Type object and it to connections ArrayList
+                            connections.add(new Connection(property, destination));
+                        }
                     }
-                    
-                    
-                  }
-                 System.out.println("Size"+connections.size());
+                //System.out.println("Size"+connections.size());
                 }
-
             }
-
-
         } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
-
+    //Constructor of the Class
+    /*
+     * 
+     * openFile the path of the Nib File That will Parse
+     * 
+     * saveFile the path of the file That will use to Save the Generated Style
+     * 
+     */
     GeneratorStyle(String openFile, String saveFile) {
         // Assume filename argument
         basename = FilenameUtils.getBaseName(openFile).toUpperCase();
+        //Generate the Connections that will use to Generate Style
         GeneratorConnection(openFile);
         String styleCode = "";
         try {
             // Build the document with SAX and Xerces, no validation
             SAXBuilder builder = new SAXBuilder();
-            // Create the document
-//			org.jdom2.Document document = builder
-//					.build(new File("ViewTest.xml"));
+            //open the nib file to get all the connections of Outlets
             org.jdom2.Document document = builder.build(new File(openFile));
-
-            // System.out.print(openFile);
+            // get the root of the nib file "Root Tag of XML"
             Element root = document.getRootElement();
+            //Make Filter that use to search for Tag his name is "subviews"
             ElementFilter filter = new org.jdom2.filter.ElementFilter(
                     "subviews");
-            XMLOutputter fmt = new XMLOutputter();
             List<Element> uiElements = null;
+            //Loop in All Tags That mathch The Filter 
             for (Element c : root.getDescendants(filter)) {
-
+            //get the Childern of the echc Subviews Tag "The controls That make Style for it UIButton UILable UiTextFeild"
                 uiElements = c.getChildren();
-
-
-
                 for (int count = 0; count < uiElements.size(); count++) {
-
+             //get the id of the Control and the Check if it Match the Destenation Variable in Connection Object 
+             //then using the Match Connection object and use Property Variable of it.
                     String id = uiElements.get(count).getAttributeValue("id");
                     String name = uiElements.get(count).getName();
                     for (Connection connection : connections) {
-
-                        String property = connection.propertyName;
+            //Check the Connection Matching of Control
                         if (connection.destination.equals(id)) {
-                            System.out.print(name.toUpperCase() + "   " + property + "   \n");
+                            String property = connection.propertyName;
+                            //System.out.print(name.toUpperCase() + "   " + property + "   \n");
+                            //Send the Match Control to setProperties Function That Generate the Style and Return String have Style
                             String result = setProperties(
                                     name.toUpperCase(),
                                     property.toUpperCase(), uiElements.get(
                                     count));
-
+                            //Because We not Support All UI We Return "not" when the Control is not Supported
                             if (!result.equals("not")) {
                                 //System.out.print(styleCode);
                                 styleCode += result + "\n ==============================================\n";
@@ -142,61 +146,40 @@ public class GeneratorStyle {
             String result4 = "#define " + basename + "_" + name + "_LABEL_BACKGROUND_COLOR 0XFFFFFF";
             String result5 = "";
 
-
-
             if (element.getAttribute("numberOfLines") != null) {
                 result5 = "#define " + basename + "_" + name + "_LABEL_NO_OF_LINES " + element.getAttributeValue("numberOfLines");
             } else {
                 result5 = "#define " + basename + "_" + name + "_LABEL_NO_OF_LINES 1";
             }
+            
             return result1 + ";\n" + result2 + ";\n" + result3 + ";\n" + result4 + ";\n" + result5;
 
         } else if (typeUI.equalsIgnoreCase("textField")) {
 
-//            String result1 = "#define " + basename + "_" + name + "_TEXT_FIELD_NAME @\"Arial\"";
-//            String result2 = "#define " + basename + "_" + name + "_TEXT_FIELD_SIZE 14";
 
             String result1 = "#define " + basename + "_" + name + "_TEXT_FIELD_NAME @\"" + element.getChild("fontDescription").getAttributeValue("name") + "\"";
             String result2 = "#define " + basename + "_" + name + "_TEXT_FIELD_SIZE " + element.getChild("fontDescription").getAttributeValue("pointSize");
-
-
-
             String result3 = "#define " + basename + "_" + name + "_TEXT_FIELD_COLOR 0x000000";
-//            String result4 = "#define " + basename + "_" + name + "_TEXT_FIELD_HEIGHT 30";
-//            String result5 = "#define " + basename + "_" + name + "_TEXT_FIELD_WIDTH 300";
-
             String result4 = "#define " + basename + "_" + name + "_TEXT_FIELD_HEIGHT " + element.getChild("rect").getAttributeValue("height");
             String result5 = "#define " + basename + "_" + name + "_TEXT_FIELD_WIDTH " + element.getChild("rect").getAttributeValue("width");
-
 
             return result1 + ";\n" + result2 + ";\n" + result3 + ";\n" + result4 + ";\n" + result5;
 
         } else if (typeUI.equalsIgnoreCase("button")) {
 
-//            String result1 = "#define " + basename + "_" + name + "_BUTTON_TITLE_FONT_NAME @\"Arial\"";
-//            String result2 = "#define " + basename + "_" + name + "_BUTTON_TITLE_FONT_SIZE 14";
-
             String result1 = "#define " + basename + "_" + name + "_BUTTON_TITLE_FONT_NAME @\"" + element.getChild("fontDescription").getAttributeValue("name") + "\"";
             String result2 = "#define " + basename + "_" + name + "_BUTTON_TITLE_FONT_SIZE " + element.getChild("fontDescription").getAttributeValue("pointSize");
-
             String result3 = "#define " + basename + "_" + name + "_BUTTON_TITLE_NORMAL_TEXT_COLOR 0x157DEC";
             String result4 = "#define " + basename + "_" + name + "_BUTTON_TITLE_DISABLED_TEXT_COLOR 0x157DEC";
             String result5 = "#define " + basename + "_" + name + "_BUTTON_TITLE_SELECTED_TEXT_COLOR 0x157DEC";
+            
             return result1 + ";\n" + result2 + ";\n" + result3 + ";\n" + result4 + ";\n" + result5;
 
         } else if (typeUI.equalsIgnoreCase("textview")) {
-//            String result1 = "#define " + basename + "_" + name + "_TEXTVIEW_FONT_NAME @\"Arial\"";
-//            String result2 = "#define " + basename + "_" + name + "_TEXTVIEW_FONT_SIZE 14";
+
             String result1 = "#define " + basename + "_" + name + "_TEXTVIEW_FONT_NAME @\"" + element.getChild("fontDescription").getAttributeValue("name") + "\"";
             String result2 = "#define " + basename + "_" + name + "_TEXTVIEW_FONT_SIZE " + element.getChild("fontDescription").getAttributeValue("pointSize");
-
             String result3 = "#define " + basename + "_" + name + "_TEXTVIEW_COLOR 0x333333";
-
-
-
-//            String result4 = "#define " + basename + "_" + name + "_TEXTVIEW_HIEGHT 30";
-//            String result5 = "#define " + basename + "_" + name + "_TEXTVIEW_WIDTH 250";
-
             String result4 = "#define " + basename + "_" + name + "_TEXTVIEW_HIEGHT " + element.getChild("rect").getAttributeValue("height");
             String result5 = "#define " + basename + "_" + name + "_TEXTVIEW_WIDTH " + element.getChild("rect").getAttributeValue("width");
 
